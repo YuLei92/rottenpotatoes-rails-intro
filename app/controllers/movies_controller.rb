9cca1eed286add7153e87b1c286be8b6e02b922d
YuLei92 <medicily@hotmail.com>
2018-03-02 00:21:52 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.order(params[:sort_by])
     if params[:ratings]
-      @movies = Moive.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
     end
     @sort_column = params[:sort_by]
-    @all_ratings = Moive.all_ratings
+    @all_ratings = Movie.all_ratings
     @set_ratings = params[:ratings]
     if !@set_ratings
       @set_ratings = Hash.new
